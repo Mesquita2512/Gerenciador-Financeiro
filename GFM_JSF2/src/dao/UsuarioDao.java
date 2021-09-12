e86398a69adcb4916705b997d4175f9777a53020
Mesquita2512 <54009128+Mesquita2512@users.noreply.github.com>
2021-09-12 20:37:32 -0300

    pequenas correcoes

--- GFM_JSF2/src/dao/UsuarioDao.java
+++ GFM_JSF2/src/dao/UsuarioDao.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.transaction.TransactionalException;
-
 import entity.Usuario;
 import fabricaConexao.FabricaJpa;
 
