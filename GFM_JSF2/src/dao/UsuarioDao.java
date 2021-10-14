093b222db6fbfa9267e98313554cef78ce76e8f4
Mesquita2512 <54009128+Mesquita2512@users.noreply.github.com>
2021-10-13 23:47:21 -0300

    Erros

--- GFM_JSF2/src/dao/UsuarioDao.java
+++ GFM_JSF2/src/dao/UsuarioDao.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.transaction.TransactionalException;
+
 import entity.Usuario;
 import fabricaConexao.FabricaJpa;
 
